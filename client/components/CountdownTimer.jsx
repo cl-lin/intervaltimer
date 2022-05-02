0ade13b99c9ec3f4a0475f8a3f38e5d877a5d7db
jinny <vedaforest@gmail.com>
2022-05-02 22:05:47 +1200

    yeah~ only use onChange and useState

--- client/components/CountdownTimer.jsx
+++ client/components/CountdownTimer.jsx
@@ -1,15 +1,18 @@
-import React, { useState } from 'react'
-import useCountDown from 'react-countdown-hook'
+import React, { useState } from "react";
+import useCountDown from "react-countdown-hook";
 
 // const initialTime = 40 * 1000
 
 const CountdownTimer = () => {
-  const [seconds, setSeconds] = useState(0)
+  const [seconds, setSeconds] = useState(0);
 
-  const initialTime = seconds // initial time in milliseconds, defaults to 60000
-  const interval = 1000 // interval to change remaining time amount, defaults to 1000
+  // const initialTime = seconds // initial time in milliseconds, defaults to 60000
+  const interval = 1000; // interval to change remaining time amount, defaults to 1000
 
-  const [timeLeft, { start, pause, resume, reset }] = useCountDown(initialTime, interval)
+  const [timeLeft, { start, pause, resume, reset }] = useCountDown(
+    seconds,
+    interval
+  );
 
   // start the timer during the first render
   // React.useEffect(() => {
@@ -21,39 +24,29 @@ const CountdownTimer = () => {
   //   start(newTime)
   // }, [])
 
-  const handleSubmit = () => {
-    const time = seconds * 1000
-    console.log('input', time)
-  }
+  const handleChange = (e) => {
+    const time = e.target.value * 1000;
+    setSeconds(time);
+  };
+
 
   return (
     <>
       <p>Input your time (in seconds):</p>
-      <input
-        value = {seconds}
-        onChange = {(e) => setSeconds(e.target.value)}
-      ></input>
-      <button onClick={handleSubmit}>Submit</button>
+      <input onChange={handleChange} />
+      <button onClick={() => start(seconds)}>Submit</button>
 
       <p>Time left: {timeLeft / 1000}</p>
 
-      <button onClick={() => start()}>
-        Start
-      </button>
+      <button onClick={() => start(seconds)}>Start</button>
       {/* <button onClick={restart}>
         Restart counter with 40 seconds
       </button> */}
-      <button onClick={() => pause()}>
-        Pause
-      </button>
-      <button onClick={() => resume()}>
-        Resume
-      </button>
-      <button onClick={() => reset()}>
-        Reset
-      </button>
+      <button onClick={() => pause()}>Pause</button>
+      <button onClick={() => resume()}>Resume</button>
+      <button onClick={() => reset()}>Reset</button>
     </>
-  )
-}
+  );
+};
 
-export default CountdownTimer
+export default CountdownTimer;
